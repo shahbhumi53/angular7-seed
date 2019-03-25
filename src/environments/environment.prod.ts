b7986e65e7e3152bca2c3c6fd7385339941273a6
shahbhumi53 <shahbhumi53@gmail.com>
2019-03-25 14:14:12 +0530

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+const getHost = () => {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
+  return `${protocol}//${host}`;
+};
+const domain = 'prospectbird.auth0.com';
+const clientId = 'onVq1L51x1i11Qs2R7D3Ji2B8Fibexjt';
+const stripeKey = 'pk_live_aXANVTDYZw8xewS7ETP24lmu';
+const apiURI = 'https://crmgang.com/';
 export const environment = {
-  production: true
+  production: true,
+  BASE_URI: getHost(),
+  BASE_API: apiURI,
+  CLIENT_ID: clientId,
+  STRIPE_KEY: stripeKey,
+  DOMAIN: domain
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,23 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const getHost = () => {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
+  return `${protocol}//${host}`;
+};
+const domain = 'prospectbird.auth0.com';
+const apiURI = 'https://crmgang.com/';
+const clientId = 'hMnBewO9tcLMqM1_yusPFDKUrNdWr5jm';
+const stripeKey = 'pk_test_gPA51N21tmN6tYXdAa48hdZB';
+
 export const environment = {
-  production: false
+  production: false,
+  BASE_URI: getHost(),
+  BASE_API: apiURI,
+  CLIENT_ID: clientId,
+  STRIPE_KEY: stripeKey,
+  DOMAIN: domain
 };
 
 /*
